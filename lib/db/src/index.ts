ed23af559b6d5b439f1c0ca046295acab50d5679
Everton Souza <evertonsouzabrpt@gmail.com>
2026-04-26 18:00:41 +0100

    Update index.ts

--- lib/db/src/index.ts
+++ lib/db/src/index.ts
@@ -1,16 +1,8 @@
-import { drizzle } from "drizzle-orm/node-postgres";
-import pg from "pg";
-import * as schema from "./schema";
+import app from "./app";
+import { logger } from "./lib/logger";
 
-const { Pool } = pg;
+const port = process.env.PORT || 3000;
 
-if (!process.env.DATABASE_URL) {
-  throw new Error(
-    "DATABASE_URL must be set. Did you forget to provision a database?",
-  );
-}
-
-export const pool = new Pool({ connectionString: process.env.DATABASE_URL });
-export const db = drizzle(pool, { schema });
-
-export * from "./schema";
+app.listen(port, () => {
+  logger.info({ port }, "Server running locally");
+});
